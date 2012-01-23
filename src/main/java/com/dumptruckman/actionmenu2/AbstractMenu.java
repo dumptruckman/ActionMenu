17df0c170fa7c7c3541de2a89815cb3248bdc1f8
Jeremy Wood <farachan@gmail.com>
2012-01-23 16:27:25 -0500

    added @Override

--- src/main/java/com/dumptruckman/actionmenu2/AbstractMenu.java
+++ src/main/java/com/dumptruckman/actionmenu2/AbstractMenu.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 public abstract class AbstractMenu implements Menu {
     
     MenuContents<MenuItem> contents = new DefaultMenuContents<MenuItem>(new ArrayList<MenuItem>());
-    
+
+    @Override
     public MenuContents<? extends MenuItem> getContents() {
         return this.contents;
     }
-    
+
+    @Override
     public MenuItem getSelectedMenuItem() {
         return this.getContents().get(this.getContents().getSelectedIndex());
     }
+
+
 }
