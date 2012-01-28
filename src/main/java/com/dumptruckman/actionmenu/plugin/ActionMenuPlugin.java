19857d31758c83ae3208bf9d05e595a34635533e
Jeremy Wood <farachan@gmail.com>
2012-01-28 14:28:50 -0500

    Weird git issue.

--- src/main/java/com/dumptruckman/actionmenu/plugin/ActionMenuPlugin.java
+++ src/main/java/com/dumptruckman/actionmenu/plugin/ActionMenuPlugin.java
@@ -1,61 +1,50 @@
 package com.dumptruckman.actionmenu.plugin;
 
 import com.dumptruckman.actionmenu.ActionMenu;
-import com.dumptruckman.actionmenu.map.MapActionMenu;
-import com.dumptruckman.actionmenu.map.MapActionMenuItem;
-import org.bukkit.entity.Fireball;
-import org.bukkit.plugin.PluginManager;
+import com.dumptruckman.actionmenu2.api.MenuHandle;
+import com.dumptruckman.actionmenu2.api.MenuItem;
+import com.dumptruckman.actionmenu2.api.Menus;
+import com.dumptruckman.actionmenu2.api.event.ActionEvent;
+import com.dumptruckman.actionmenu2.api.event.ActionListener;
+import com.dumptruckman.actionmenu2.api.view.MenuView;
+import com.dumptruckman.actionmenu2.impl.SimpleMenuItem;
+import org.bukkit.Bukkit;
+import org.bukkit.block.Sign;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
 
-import java.util.logging.Logger;
+public class ActionMenuPlugin extends JavaPlugin implements Listener {
 
-/**
- * A plugin class for testing the ActionMenu API.
- */
-public class ActionMenuPlugin extends JavaPlugin {
+    MenuHandle menuHandle = null;
 
-    private ActionMenu menu;
-
-    /**
-     * The log for this plugin.
-     */
-    public static final Logger LOG = Logger.getLogger("Minecraft.ActionMenu");
-
-    @Override
     public void onEnable() {
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(new ActionMenuListener(this), this);
-        //pm.registerEvent(Event.Type.PLAYER_ITEM_HELD, listener, Event.Priority.Normal, this);
-        menu = new MapActionMenu();
-        menu.setHeader("Spells");
-        menu.add(new MapActionMenuItem("Fireball") {
-            public void run() {
-                final Vector direction = getPlayer().getEyeLocation().getDirection().multiply(2);
-                getPlayer().getWorld().spawn(getPlayer().getEyeLocation().add(direction.getX(), direction.getY(),
-                        direction.getZ()), Fireball.class);
-            }
-        });
-        menu.add(new MapActionMenuItem("This is a plugin of the emergency broadcast system.") {
-            public void run() {
-            }
-        });
-        menu.add(new MapActionMenuItem("12345678901234567890123456789012345678901234567890") {
-            public void run() {
-            }
-        });
-        LOG.info(this + " enabled.");
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @Override
     public void onDisable() {
-        LOG.info(this + " disabled.");
+
     }
 
-    /**
-     * @return The Menu object for this test plugin.
-     */
-    public ActionMenu getMenu() {
-        return this.menu;
+    @EventHandler
+    public void signChange(SignChangeEvent event) {
+        if (event.getLine(0).contains("menu")) {
+            menuHandle = Menus.getMenuHandle((Sign) event.getBlock().getState());
+            MenuItem test = new SimpleMenuItem();
+            test.setText("test");
+            test.getListeners().add(new TestActionListener());
+            MenuItem poop = new SimpleMenuItem();
+            poop.setText("poop");
+
+            menuHandle.getMenu().getContents().add(test);
+            menuHandle.getMenu().getContents().add(poop);
+        }
+    }
+
+    private class TestActionListener implements ActionListener {
+        public void onAction(ActionEvent event) {
+
+        }
     }
 }
